831dcd8e9578e7bd1c5602aa614045e0665d0c9a
_Chenbing <muzi@muzilib.com>
2023-01-01 23:16:49 +0800

    chore(初始化项目): 添加测试方法

--- src/main/java/com/c301/modules/controller/HelloController.java
+++ src/main/java/com/c301/modules/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.c301.modules.controller;
 
 import com.c301.modules.plugin.base.annotation.ApiVersion;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +24,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/hello-world")
     public String hello() {
         return "Hello world";
+    }
+
+    @GetMapping("/{say}")
+    public String say(@PathVariable("say") String say) {
+        return say;
+    }
+
+    @GetMapping("/file")
+    public Resource hello1() {
+        return new FileSystemResource("D:/图片/EC83488719EDDFF991B94A75761DBD19.jpg");
     }
 
 }
