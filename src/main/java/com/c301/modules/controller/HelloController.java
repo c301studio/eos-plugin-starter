c80359fcc6bb4dc520c10b292544fa466de23436
_Chenbing <muzi@muzilib.com>
2023-01-04 23:34:46 +0800

    chore(接口): 添加文档注解

--- src/main/java/com/c301/modules/controller/HelloController.java
+++ src/main/java/com/c301/modules/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.c301.modules.controller;
 
 import com.c301.modules.plugin.base.annotation.ApiVersion;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2022/12/28 15:08
  * @Version 1.0
  **/
+@Api(tags = "Hello接口")
 @Controller
 @ApiVersion(value = "/ap1")
 @RestController
@@ -23,11 +26,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello-world")
+    @ApiOperation("普通接口")
     public String hello() {
         return "Hello world";
     }
 
     @GetMapping("/{say}")
+    @ApiOperation("Say接口")
     public String say(@PathVariable("say") String say) {
         return say;
     }
